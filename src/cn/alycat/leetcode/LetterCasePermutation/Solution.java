7af6d97826b0d6697fee049ef93549fe21fcd241
alycat <lyanbj@cn.ibm.com>
2019-03-12 20:04:18 +0800

    init

--- src/cn/alycat/leetcode/LetterCasePermutation/Solution.java
+++ src/cn/alycat/leetcode/LetterCasePermutation/Solution.java
@@ -1,7 +1,10 @@
 package cn.alycat.leetcode.LetterCasePermutation;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class Solution {
 
@@ -27,5 +30,50 @@ public class Solution {
         if (le > 64 && le < 91) {
             a[idx] = (char)(le+32);
         }
+    }
+    // METHOD SIGNATURE BEGINS, THIS METHOD IS REQUIRED
+    public static int minimumCostIncurred(int numTotalEdgeNodes,
+                            int numTotalAvailableNetworkRoutes,
+                            List<List<Integer>> networkRoutesAvailable,
+                            int numNewNetworkRoutesConstruct,
+                            List<List<Integer>> costNewNetworkRoutesConstruct)
+    {
+        // WRITE YOUR CODE HERE
+        if(numTotalEdgeNodes == 0 && numNewNetworkRoutesConstruct == 0 ){return -1;}
+        Map<Integer,Integer> map = new HashMap<>();
+        for(List<Integer> route:networkRoutesAvailable){
+            if(!map.containsKey(route.get(0))){
+                map.put(route.get(0),route.get(1));
+            }else if(!map.containsKey(route.get(1))){
+                map.put(route.get(1),route.get(0));
+            }
+        }
+        int path = 0;
+        for(List<Integer> route:costNewNetworkRoutesConstruct){
+            if(!map.containsKey(route.get(0)) && !map.containsValue(route.get(0))){
+                path +=route.get(2);
+            }else if(map.containsKey(route.get(0)) && !map.containsValue(route.get(1))){
+                path +=route.get(2);
+            }
+        }
+        return path;
+    }
+    public static void main(String[] args) {
+       List<List<Integer>> networkRoutesAvailable =new ArrayList<>();
+
+        List<Integer> a1 = new ArrayList<>();
+        a1.add(1);
+        a1.add(2);
+        List<Integer> a2 = new ArrayList<>();
+        a2.add(3);
+        a2.add(4);
+        List<Integer> a3 = new ArrayList<>();
+        a3.add(4);
+        a3.add(5);
+        List<Integer> a4 = new ArrayList<>();
+        a4.add(3);
+        a4.add(5);
+        List<List<Integer>> costNewNetworkRoutesConstruct = new ArrayList<>();
+
     }
 }
